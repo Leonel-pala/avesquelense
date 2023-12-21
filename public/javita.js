c08e06d775e39dc8b8e0ac3e27501c371b434378
Leonel Gonzalez <leonelpala234@gmail.com>
2023-12-21 02:47:13 -0300

    v1.0.0

---
+++ public/javita.js
@@ -0,0 +1,36 @@
+/*
+<article>
+    <span class="stick"></span>
+        <img src="../style/img/birds/loica.webp">
+            <h5>Loica</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae praesentium eveniet harum cumque maiores, assumenda facilis in vitae quam esse deleniti veniam minima cum voluptates aliquid sapiente fugiat nisi similique.</p>
+    <input type="button" value="Mas detalles" class="btn">
+</article>
+         */
+window.addEventListener('load', e=>{
+    let enciclo = document.querySelector('#enciclo')
+    fetch('../base.json')
+    .then(response => response.json())
+    .then(data =>{
+        let largo = Object.keys(data).length;
+        for (let i = 1; i <= largo ; i++) {
+                enciclo.innerHTML += `<article id="${data[i].nombre}">
+                <span class="reference">${i}.</span>
+                <span class="stick"></span>
+                    <img class="birdIMG" src="${data[i].url}">
+                        <h5 class="birdNAME">${data[i].nombre}</h5>
+                    <p class="birdDES">${data[i].descripcion}</p>
+                <input class="birdBTN"type="button" value="Mas detalles" class="btn">
+            </article>`    
+            }
+    })
+    let search = document.querySelector('#search')
+    search.addEventListener('input', e=>{
+
+        document.querySelectorAll('article').forEach(ave =>{
+            ave.id.toLocaleLowerCase().includes(search.value.toLocaleLowerCase())
+            ?ave.classList.remove('filtro')
+            :ave.classList.add('filtro')    
+        })
+    })
+})
